75cfff74ccf3c04b445e81ccf762c8aaa8bbbb73
Anthony Matos <anthonymatos2134@gmail.com>
2020-03-08 16:04:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/customerserviceupload/Constants.java
@@ -0,0 +1,6 @@
+package com.example.customerserviceupload;
+
+public class Constants {
+    public static final String UPLOAD_URL = "https://ec2-3-17-70-159.us-east-2.compute.amazonaws.com/UploadExample/upload.php";
+    public static final String IMAGES_URL = "http://ec2-3-17-70-159.us-east-2.compute.amazonaws.com/UploadExample/getImages.php";
+}
